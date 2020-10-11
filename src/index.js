2dc869828e998b95c96066cc4b685fe79b82d55e
Ayhan Eraslan <ayer50@gmail.com>
2020-10-11 23:36:23 +0300

    helper, flatten, unique, getColumn

--- src/index.js
+++ src/index.js
@@ -223,39 +223,4 @@ class _builder{
     }
 }
 
-let o = [
-    {
-        make: "audi",
-        model: "r8",
-        year: "2012",
-        test:{a:8, b:15, x:{r:18, h:455}}
-    },
-    {
-        make: "audi",
-        model: "rs5",
-        year: "2013"
-    },
-    {
-        make: "ford",
-        model: "mustang",
-        year: "2012"
-    },
-    {
-        make: "ford",
-        model: "fusion",
-        year: "2015"
-    },
-    {
-        make: "kia",
-        model: "optima",
-        year: "2012"
-    }];
-
-let collect = new dcollect(o);
-let filter = collect.query().flatten();
-console.log(filter);
-
-
-
-
 module.exports = dcollect;
